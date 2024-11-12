5092f3d43705e873d0ecb78b1fbd8052d97c5be5
HP <vishalmorey1998@gmail.com>
2024-11-12 12:54:32 +0530

    by piyush

--- javaB181/src/com/cjc/model/Demo.java
+++ javaB181/src/com/cjc/model/Demo.java
@@ -1,6 +1,10 @@
 package com.cjc.model;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("this is M1...........");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is a Github Method");

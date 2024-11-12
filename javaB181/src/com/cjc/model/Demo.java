febe001216bbfc2e224270f488afdc7242c42bea
vishalmorey7620 <vishalmorey7878@gmail.com>
2024-11-12 13:00:27 +0530

    Update Demo.java

--- javaB181/src/com/cjc/model/Demo.java
+++ javaB181/src/com/cjc/model/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
 	{
 		System.out.println("this is M1...........");
 	}
+	public void m2()
+	{
+		System.out.println("this is M2...........");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is a Github Method");
